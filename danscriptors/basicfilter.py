34a82c99dc0e98c102cc94b3cd8d8f8653ff5049
howthebodyworks <git@email.possumpalace.org>
2016-09-09 14:02:20 +1000

    scipy filtering routines

---
+++ danscriptors/basicfilter.py
@@ -0,0 +1,77 @@
+import numpy as np
+from math import exp, log
+from scipy.signal import filtfilt, decimate, iirfilter
+    
+def RC(Wn, btype='low', dtype=np.float64):
+     """old-fashioned minimal filter design, if you don't want this modern Bessel nonsense
+     C'n'C SC:
+     coef = exp(-2*pi * (freq * SampleDur.ir));
+     out(i) = ((1 - abs(coef)) * in(i)) + (coef * out(i-1)).
+     """
+     epsilon = np.finfo(dtype).eps
+     f = Wn * 2.0 #scipy convention is to count frequency up to nyquist; we double it to get 2pi
+     x = exp(-2*np.pi*f)
+     if btype == 'low':
+         b, a = np.zeros(2), np.zeros(2)
+         b[0] = 1.0 - x - epsilon #filter instability
+         b[1] = 0.0
+         a[0] = 1.0
+         a[1] = -x
+     elif btype == 'high':
+         b, a = np.zeros(2), np.zeros(2)
+         b[0] = (1.0+x)/2.0
+         b[1] = -(1.0+x)/2.0
+         a[0] = 1.0
+         a[1] = -x
+     else:
+         raise ValueError("btype must be 'low' or 'high'")
+     return b,a
+
+def notch(Wn, bandwidth):
+    """
+    Notch filter to kill line-noise.
+    """
+    f = Wn * 2.0
+    R = 1.0 - 3.0*(bandwidth/2.0)
+    K = ((1.0 - 2.0*R*np.cos(2*np.pi*f) + R**2)/(2.0 - 
+    2.0*np.cos(2*np.pi*f)))
+    b,a = np.zeros(3),np.zeros(3)
+    a[0] = 1.0
+    a[1] = - 2.0*R*np.cos(2*np.pi*f)
+    a[2] = R**2
+    b[0] = K
+    b[1] = -2*K*np.cos(2*np.pi*f)
+    b[2] = K
+    return b,a
+
+def narrowband(Wn, bandwidth):
+    """
+    Narrow-band filter to isolate a single frequency.
+    """
+    f = Wn * 2.0
+    R = 1.0 - 3.0*(bandwidth/2.0)
+    K = ((1.0 - 2.0*R*np.cos(2*np.pi*f) + R**2)/(2.0 - 
+    2.0*np.cos(2*np.pi*f)))
+    b,a = np.zeros(3),np.zeros(3)
+    a[0] = 1.0
+    a[1] = - 2.0*R*np.cos(2*np.pi*f)
+    a[2] = R**2
+    b[0] = 1.0 - K
+    b[1] = 2.0*(K-R)*np.cos(2*np.pi*f)
+    b[2] = R**2 - K
+    return b,a
+
+def high_passed(wavdata, sr=22050.0, f=20.0):
+    """remove the bottom few Hz (def 20Hz)"""
+    rel_f = 0.5 * f/float(sr)
+    b, a = RC(Wn=rel_f, btype='high')
+    #NB filtfilt seems to return fortan-contiguous arrays somehow?
+    return np.ascontiguousarray(filtfilt(b, a, wavdata))
+
+def normalized(wavdata, norm=1):
+    """
+    Centered, normalized array.
+    """
+    wavdata -= wavdata.mean()
+    wavdata *= 1.0/(np.abs(wavdata)**norm).max()
+    return wavdata
